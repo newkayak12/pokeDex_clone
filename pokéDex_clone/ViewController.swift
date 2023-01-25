95f4cb47a6cd355298798d9590381682b3f28a0f
newkayak12 <newkayak12@gmail.com>
2023-01-25 22:21:56 +0900

    init

---
+++ pokéDex_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokéDex_clone
+//
+//  Created by Sang Hyeon kim on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
