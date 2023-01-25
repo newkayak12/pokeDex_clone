2a15808c3ecc0e98f15f9c514013f1ef796e1efe
newkayak12 <newkayak12@gmail.com>
2023-01-25 22:30:17 +0900

    init

---
+++ pokéDex_clone/model/poke.swift
@@ -0,0 +1,14 @@
+//
+//  poké.swift
+//  pokéDex_clone
+//
+//  Created by Sang Hyeon kim on 2023/01/25.
+//
+
+import Foundation
+class poke: Codable {
+    private var mainImage: Data
+    private var no: String
+    private var name: String
+    private var type: [String]
+}
